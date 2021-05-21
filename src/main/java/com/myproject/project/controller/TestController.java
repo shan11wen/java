349a0e19b6d17c71ed75263987615f211ba61265
shanwen.xiong <shanwen.xiong@mobilemd.cn>
2021-05-21 15:34:45 +0800

    web first commit

---
+++ src/main/java/com/myproject/project/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.myproject.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName TestController
+ * @description:
+ * @author: xiongshanwen
+ * @create: 2021-05-21 15:30
+ **/
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "success";
+    }
+
+
+}
